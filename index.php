fa3df70a03533fe9a21a34c500b6ee790b6bd317
Nagarjunaracakunta <nagarjunarachakunta@gmail.com>
2018-02-26 23:17:16 +0530

    my first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<meta charset="UTF-8">
+<title>My HTML5 Webpage</title>
+<script src="javascript/script.js"></script>
+<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<header>
+Welcome to My Webpage
+</header>
+  <nav>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Blog</a></li>
+      <li> <a href="#">About</a></li>
+      <li> <a href="#">Contact</a></li>
+    </ul>
+  </nav>
+  <section>
+  	<h1>Registration Form</h1>
+  </section>
+  <footer>copyright @ 2018</footer>
+</body>
+
+</html>
+
